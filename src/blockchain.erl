baf1463e01ce0092b4349d7a9a6ff745f2139855
Sergey Abramyan <sa.abramyan@gmail.com>
2015-10-01 15:37:50 +0300

    Add get_balance and send

--- src/blockchain.erl
+++ src/blockchain.erl
@@ -1,56 +1,81 @@
 -module('blockchain').
 
 %% API exports
--export([create_wallet/2]).
+-export([create_wallet/1]).
+-export([get_balance/2]).
+-export([send/5]).
 
 -define(BLOCKCHAIN_BASE_URL, "https://blockchain.info/ru/api/v2").
+-define(MERCHANT_BASE_URL, "https://blockchain.info/ru/merchant").
 -define(CONTENT_TYPE, "application/x-www-form-urlencoded").
 -define(PASSWORD_LENGTH, 10).
+-define(DIVIDE_BY, 100000000).
 
--type action() :: create_wallet.
+-type password() :: binary().
+-type address() :: binary().
+-type amount() :: float().
+-type guid() :: binary().
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
--spec create_wallet(binary(), binary()) -> any().
-create_wallet(Password, ApiCode) when is_binary(Password),
-                                      is_binary(ApiCode) ->
+-spec create_wallet(password()) -> any().
+create_wallet(Password) when is_binary(Password) ->
     case size(Password) < ?PASSWORD_LENGTH of
         true -> {error, password_should_be_longer};
         false ->
-            Data = [{<<"password">>, Password}, {<<"api_code">>, ApiCode}],
-            request(create_wallet, Data)
+            Data = [{<<"password">>, Password}],
+            request(post, url(create_wallet), Data)
     end.
 
-request(create_wallet, Data) ->
-    request(post, url(create_wallet), Data).
+-spec get_balance(guid(), password()) -> any().
+get_balance(Guid, Password) ->
+    request(get, url({get_balance, Guid, Password}), []).
 
-request(post, Url, Data) ->
-    {ok, {{_, Status, _}, _Headers, Body}} = do_post(Url, Data),
-    case Status of
-        500 ->
-            {error, Body};
-        _Any ->
-            {ok, Body}
-    end.
+-spec send(guid(), password(), address(), address(), amount()) -> any().
+send(Guid, Password, From, To, Amount) ->
+    request(post, url({send, Guid, Password, From, To, Amount}), []).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
--spec url(action()) -> binary().
-url(create_wallet) ->
-    iolist_to_binary([?BLOCKCHAIN_BASE_URL, "/create_wallet"]).
-
--spec do_post(binary(), list()) -> any().
-do_post(Url, Data) when is_binary(Url) ->
+request(Method, Url, Data) ->
     UrlList = binary_to_list(Url),
-    httpc:request(post, {UrlList, [], ?CONTENT_TYPE, form_data(Data)}, [], []).
+    Result = case Method of
+                 get -> httpc:request(UrlList);
+                 post ->
+                     httpc:request(Method, {UrlList, [], ?CONTENT_TYPE, form_data(Data)}, [], [])
+             end,
+    {ok, {{_, Status, _}, _Headers, Body}} = Result,
+    case Status of
+        500 ->
+            {error, Body};
+        _Any ->
+            BodyBin = list_to_binary(Body),
+            {ok, jsx:decode(BodyBin)}
+    end.
+
+api_code() ->
+    {ok, ApiCode} = application:get_env(?MODULE, api_code),
+    list_to_binary(ApiCode).
+
+url(create_wallet) ->
+    iolist_to_binary([?BLOCKCHAIN_BASE_URL, "/create_wallet"]);
+url({get_balance, Guid, Password}) ->
+    iolist_to_binary([?MERCHANT_BASE_URL, "/", Guid,
+                      "/balance?password=", Password, "&api_code=", api_code()]);
+url({send, Guid, Password, From, To, Amount}) ->
+    iolist_to_binary([?MERCHANT_BASE_URL, "/", Guid, "/payment",
+                      "?password=", Password,
+                      "&from=", From, "&to=", To,
+                      "&amount=", integer_to_binary(Amount)]).
 
 -spec form_data(list()) -> binary().
 form_data(Data) ->
-    form_data(Data, []).
+    Data2 = [{<<"api_code">>, api_code()} | Data],
+    form_data(Data2, []).
 
 form_data([], Acc) ->
     iolist_to_binary(lists:reverse(Acc));
@@ -58,4 +83,3 @@ form_data([{Key, Value}], Acc) ->
     form_data([], [<<Key/binary, "=", Value/binary>> | Acc]);
 form_data([{Key, Value} | Rest], Acc) ->
     form_data(Rest, [<<Key/binary, "=", Value/binary, "&">> | Acc]).
-
